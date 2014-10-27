3e6575c5a149834a8b547de4b0b0dc29acb6460e
vleango <vleango@users.noreply.github.com>
2014-10-27 16:45:42 +0900

    update for deprecated method

--- src/ios/GPlus.m
+++ src/ios/GPlus.m
@@ -8,7 +8,7 @@
 #import "GPlus.h"
 #import <GoogleOpenSource/GoogleOpenSource.h>
 
-@interface GPlus() {
+@interface GPlus()<GPPShareDelegate> {
     CDVInvokedUrlCommand *cmd;
 }
 
@@ -20,8 +20,7 @@
     cmd = command;
     GPPSignIn *signIn = [GPPSignIn sharedInstance];
     signIn.shouldFetchGooglePlusUser = YES;
-#error Fill in your Google Plus kClientId
-    signIn.clientID = @"<#kClientId#>";
+    signIn.clientID = @"1081700471293-7ki91t72sbsk9hkrj77ql8659uiqu9tq.apps.googleusercontent.com";
     signIn.scopes = @[ kGTLAuthScopePlusLogin ];
     signIn.delegate = self;
     [signIn authenticate];
@@ -31,7 +30,8 @@
                    error: (NSError *) error {
     if (error) {
         CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"not available"];
-        [self writeJavascript:[pluginResult toErrorCallbackString:cmd.callbackId]];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:cmd.callbackId];
+      
     } else {
         [self share];
     }
@@ -40,13 +40,25 @@
 - (void) share {
     
     id<GPPNativeShareBuilder> shareBuilder = [[GPPShare sharedInstance] nativeShareDialog];
+    
+    [[GPPShare sharedInstance] setDelegate:self];
+    
     // This line will fill out the title, description, and thumbnail from
     // the URL that you are sharing and includes a link to that URL.
     [shareBuilder setURLToShare:[NSURL URLWithString:cmd.arguments[0]]];
     [shareBuilder open];
+}
+
+- (void)finishedSharingWithError:(NSError *)error {
     
-    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    [self writeJavascript:[pluginResult toSuccessCallbackString:cmd.callbackId]];
+    if (error) {
+        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:cmd.callbackId];
+    }
+    else {
+        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:cmd.callbackId];
+    }
 }
 
 @end
